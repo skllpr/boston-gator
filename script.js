21f4fa3acf1e9c709f23c0a0ca445fd332c295d2
Sohil Kollipara <46583278+skllpr@users.noreply.github.com>
2020-11-15 03:18:26 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,56 @@
-/* If you're feeling fancy you can add interactivity
-    to your site with Javascript */
+function calcDistance(lat, long) {
+  if (Math.hypot(long-startLong, lat-startLat) > .000006) {
+    console.log(initialAward);
+    score = score + 60;
+    alert(score);
+    //var text = document.
+    alert('moved far')
+    initialAward.parentNode.removeChild(initialAward);
+    var el = document.createElement('a-entity');
+    let latString = 'latitude: ' + lat;
+    let longString = '; longitude: ' + long + ";";
+    let choordString = latString+longString;
+    alert(choordString);
+    el.setAttribute('gps-entity-place', 'latitude: ${lat}; longitude: ${long};');
+    el.setAttribute('src', 'https://console.echoar.xyz/query?key=withered-bonus-8514&file=15cd7302-ece9-4caa-aa27-2cf3d2d2b11e.glb');
+    el.setAttribute('scale', '5 5 5');
+    el.setAttribute('id','remove')
+    startLat = lat;
+    document.querySelector('#scenery').appendChild(el);
+    //alert(lat);
+    alert("updated el");
+    alert(el);
+    startLat = long;
+    //alert(long);
+    console.log("moved far");
+    initialAward = document.querySelector('a-entity');
+  }
+  console.log(Math.hypot(long-startLong, lat-startLat));
+  alert(Math.hypot(long-startLong, lat-startLat))
+}
+var score = 0;
+try {
+var points = 0;
+var startLat;
+var startLong;
+var initialAward = document.querySelector('#remove');
+navigator.geolocation.getCurrentPosition((position) =>{
+  startLat = position.coords.latitude;
+  startLong = position.coords.longitude;
+    console.log(initialAward);
+});
 
-// prints "hi" in the browser's dev tools console
-console.log("hi");
-  var points = 0;
-
-var origin;
 const watchID = navigator.geolocation.watchPosition((position) => {
   console.log(position.coords.latitude);
   console.log(position.coords.longitude);
-  location.setInnerHTML(position.coords)
+  //document.getElementById("lat").innerHTML=position.coords.latitude;
+  //document.getElementById("geo").innerHTML=position.coords.longitude;
+  calcDistance(position.coords.latitude, position.coords.longitude);
+  initialAward = document.querySelector('#remove');
+    alert(initialAward);
+
 });
-var location = document.getElementById('geo');
-
-
-/*function motion() {
-while(false) {
-  var north = origin;
-  north.coords.longitude = north.positioncoords.longitude + .0001;
-  var south= origin;
-  south.coords.longitude = south.coords.longitude + .0001;
-  var east = origin;
-  east.coords.latitude = east.coords.latitude + .0001;
-  var west = origin;
-  west.coords.latitude = west.coords.latitude + .0001;
-  console.log("north");
-  console.log(north);
-  console.log("south");
-  console.log(south);
-  console.log("east");
-  console.log(east);
-  console.log("west");
-  console.log(west);
-  }
-}*/
+}
+catch (err) {
+  alert(err);
+}
